9ce459e10fe1fce15a44d823d290181073852624
Grant Okawa <gokawa04@gmail.com>
2025-05-09 17:34:23 -0400

    Added tailwind styling

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return <div className="min-h-screen ">
+
+        {/* Dark Mode */}
+
+        {/* Background Aminations */}
+
+        {/* Navbar*/}
+
+        {/* Main Content*/}
+
+        {/* Footer*/}
+    </div>;
+};
